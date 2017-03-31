d15d5eaab41189cfaf4aaa7d39021c7b1dfcf45f
kben19 <kben19@student.monash.edu>
2017-03-31 20:48:37 +1100

    version2

--- app.js
+++ app.js
@@ -6,83 +6,103 @@ admin.initializeApp({
   databaseURL: "https://intruder-counter.firebaseio.com"
 });
 
-
-
 var db = admin.database();
 
-var ref = db.ref("/motionSensorData"); // channel name
+var motionRef = db.ref("motionSensorData"); // channel name
+var switchRef = db.ref("switchData");
 
-var jfive = require("johnny-five");   
+var jfive = require("johnny-five");  
+
+var board = new jfive.Board();  
 
 
 board.on("ready", function() {
     var led = new jfive.Led(13);                                                          
     var motion = new jfive.Motion(2);     
     var timer = 0; 	
-	
+	var intruderString = "";
 	var idVar = 0;
+	var motionVal = false;
+	var motStart = false;
+	var intruder = "LSLL";
 	
-	var motionVar = 'off';           //motion sensor switch
+	setInterval(function(){                                  //timer to measure length of motion
+		timer++;
+    }, 1000);
 	
-	
-
-		setInterval(function(){                                                             //timer to measure length of motion
-			timer++;
-			}, 1000);
-		socket.on('checkbox1', function(ledVar){		                                    
-            ledVar == 'on' ? led.on() : led.off();                                                                                   
-            console.log('LED is turned ', ledVar);
-            });
+	motion.on("motionstart", function(){                                                //starts timer when motion detected                                               
+		if (motionVal && motStart == false){
+			motStart = true;
+			timer = 0;
+			console.log('Motion Start');
+		}
+	});
 		
-		socket.on('checkbox2', function(motVar){                                            
-			motVar == 'on' ? motionVar = 'on' : motionVar = 'off'
-			console.log('Motion Sensor is turned ', motVar);
-			});
-		
-		
-		
-		
-		
-		motion.on("motionstart", function(){                                                //starts timer when motion detected                                               
-			if (motionVar == 'on') {
-				
-				timer = 0;
-				console.log('Motion Start');
+	motion.on("motionend", function() {             //determine length of motion when motionend event is triggered  
+		if (motionVal && motStart){
+			console.log('Motion Ended');
+			var timeDetected = timer; 
+			if (timeDetected > 8) {
+				motionRef.push({
+					id: idVar,
+					type:'longMotion',
+					time_detected: timeDetected
+						
+				});
+				intruderString += "L";
+						
+			}else{
+				motionRef.push({
+					id: idVar,
+					type:'shortMotion',
+					time_detected: timeDetected
+						
+				});
+				intruderString += "S";
 			}
-		});
-		
-		
-		motion.on("motionend", function() {                                                 //determine length of motion when motionend event is triggered             
-			if (motionVar == 'on') {
-				console.log('Motion Ended');
-				var timeDetected = timer;                                        
-				if (timeDetected > 10) {
-					
-					ref.push({
-						
-					    id: idVar,
-                      
-					    type:'longMotion'
-						
-						time: timeDetected
-					
-				    });
-					
-					
-                }else{
-					ref.push({
-                       
-					    id: idVar,
-                        
-				        type:'shortMotion',
-						
-						time: timeDetected
-                    
-					});
-				}
-				
+			if (intruderString == intruder){
+				idVar += 1;
+				motionRef.push({
+					id: idVar,
+					type:'intruderMotion',
+					time_detected: timeDetected
+				});
+				intruderString = "";
+			}else if(intruder.substring(0, intruderString.length) != intruderString){
+				intruderString = intruderString.substring(intruderString.length-1, intruderString.length);
 			}
-		}); //setInterval
-
-
+			idVar += 1;
+			motStart = false;
+		}		
+	});
+	
+	switchRef.on('child_added', function(data){
+		var value = data.val();
+		idVar = value.id;
+		if (value.type == 'led'){
+			if(value.action == 'on'){
+				led.on();
+			}else{
+				led.off();
+			}
+			console.log("LED turned " + value.action);
+		}else{
+			if(value.action == 'on'){
+				motionVal = true;
+			}else{
+				motionVal = false;
+				motStart = false;
+			}
+			console.log("Sensor turned " + value.action);
+		}	
+	});
+	
+	switchRef.on('child_removed', function(data){
+		led.off();
+		motionVal = false;
+		motStart = false;
+		idVar = 0;
+		console.log("Data resetted");
+	});
+		
 });	 //for board		
--- firebaseWebApi.js
+++ firebaseWebApi.js
@@ -1,50 +1,142 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://intruder-counter.firebaseio.com"
-});
-
-
-
-var db = admin.database();
-
-var ref = db.ref("/motionSensorData"); // channel name
-
-var array = [];
-
-var intruder = 0;
-
-ref.on('child_added',processData);  //everytime a child is added process its data
-
-
-function processData(data){
+function Fit3140() {
+	this.shortMotionText = document.getElementById('short');
+	this.longMotionText = document.getElementById('long');
+	this.motionText = document.getElementById('motion');
+	this.intruderMotionText = document.getElementById('intruder');
+	this.ledSwitch = document.getElementById('LED');
+	this.sensorSwitch = document.getElementById('Sensor');
+	this.resetButton = document.getElementById('resetButton');
+	this.idVal = 0;
 	
-	var motion = data.val();
-
-	
-	var motion_type = motion.type;
-	
-		
-	array.push(motion_type);
-		
-	if (array.length = 4){
-		
-		if ((array[0] == 'longMotion') && (array[1] == 'shortMotion') && (array[2] == 'longMotion') && (array[3] == 'longMotion'))
-				
-			intruder += 1;
-				
-			array = [];
-				
-		}else{
-				
-			var new_array = array.slice(1,4);              //remove the 1st element
-			    
-			array = new_array;
-				
-		}
-	}		
-		
+	this.ledSwitch.addEventListener('change', this.saveLedData.bind(this));
+	this.sensorSwitch.addEventListener('change', this.saveSensorData.bind(this));
+	this.resetButton.addEventListener('click', this.resetDatabase.bind(this));
+  
+	this.initFirebase();
+	this.loadMotionData();
 }
+
+  Fit3140.prototype.initFirebase = function () {
+    this.database = firebase.database();
+    this.storage = firebase.storage();
+  };
+
+
+  Fit3140.prototype.loadMotionData = function () {
+	var totalMotion = 0;
+	var longMotion = 0;
+	var shortMotion = 0;
+	var intruderMotion = 0;
+    // Reference to the /messages/ database path.
+    this.motionSensorRef = this.database.ref('motionSensorData');
+	this.switchRef = this.database.ref('switchData');
+    // Make sure we remove all previous listeners.
+    this.motionSensorRef.off();
+	this.switchRef.off();
+
+    // Loads the last 50 messages and listen for new ones.
+    var setData = function (data) {
+      var val = data.val();
+      totalMotion += 1;
+	  this.motionText.innerText = totalMotion;
+	  if (val.type == 'longMotion'){
+		longMotion += 1;
+		this.longMotionText.innerText = longMotion;
+	  }
+	  else if (val.type == 'shortMotion') {
+		shortMotion += 1;
+		this.shortMotionText.innerText = shortMotion;
+	  }
+	  else{
+		  intruderMotion += 1;
+		  this.intruderMotionText.innerText = intruderMotion;
+	  }
+	  console.log(val.type + " added.");
+    }.bind(this);
+	
+	var setSwitch = function(data) {
+		var val = data.val();
+		if (val.type == 'led'){
+			if(val.action == 'on'){
+				this.ledSwitch.checked = true;
+			}else{
+				this.ledSwitch.checked = false;
+			}
+		}else{
+			if(val.action == 'on'){
+				this.sensorSwitch.checked = true;
+			}else{
+				this.sensorSwitch.checked = false;
+			}
+		}
+		console.log(val.type + " " + val.action + " added")
+	}.bind(this);
+	
+	var resetData = function() {
+		this.ledSwitch.checked = false;
+		this.sensorSwitch.checked = false;
+		this.motionText.innerText = 0;
+		this.shortMotionText.innerText = 0;
+		this.longMotionText.innerText = 0;
+		this.intruderMotionText.innerText = 0;
+		totalMotion = 0;
+		shortMotion = 0;
+		longMotion = 0;
+		intruderMotion = 0;
+		this.idVal = 0;
+	}.bind(this);
+	
+    this.motionSensorRef.on('child_added', setData);
+    this.motionSensorRef.on('child_changed', setData);
+	this.switchRef.on('child_added', setSwitch);
+	this.switchRef.on('child_changed', setSwitch);
+	this.switchRef.on('child_removed', resetData);
+  };
+  
+  Fit3140.prototype.saveLedData = function() {
+	this.idVal += 1;
+	if (this.ledSwitch.checked) {
+		this.switchRef.push({
+		  id: this.idVal,
+		  action: 'on',
+		  type: 'led',
+		  time: Date.now()
+		});
+	}else {
+		this.switchRef.push({
+		  id: this.idVal,
+		  action: 'off',
+		  type: 'led',
+		  time: Date.now()
+		});
+	}
+  };
+  
+  Fit3140.prototype.saveSensorData = function() {
+	this.idVal += 1;
+	if (this.sensorSwitch.checked){
+		this.switchRef.push({
+		  id: this.idVal,
+		  action: 'on',
+		  type: 'sensor',
+		  time: Date.now()
+		});
+	}else{
+		this.switchRef.push({
+		  id: this.idVal,
+		  action: 'off',
+		  type: 'sensor',
+		  time: Date.now()
+		});
+	}
+  };
+  
+  Fit3140.prototype.resetDatabase = function() {
+	this.motionSensorRef.remove();
+	this.switchRef.remove();
+  };
+
+
+window.onload = function () {
+  window.Fit3140 = new Fit3140();
+};
